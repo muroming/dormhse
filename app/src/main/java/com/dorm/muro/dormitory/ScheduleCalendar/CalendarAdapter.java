189c2cbcde3f25a2138dd8e52e798186e19265b1
Mitch <ulamog345@gmail.com>
2018-09-15 19:50:43 +0300

    Finished layout for ScheduleFragment + some logic

--- app/src/main/java/com/dorm/muro/dormitory/ScheduleCalendar/CalendarAdapter.java
+++ app/src/main/java/com/dorm/muro/dormitory/ScheduleCalendar/CalendarAdapter.java
@@ -10,6 +10,9 @@ import android.widget.TextView;
 
 import com.dorm.muro.dormitory.R;
 
+import org.joda.time.DateTimeComparator;
+
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -18,11 +21,12 @@ public class CalendarAdapter extends ArrayAdapter<Date> {
     private LayoutInflater inflater;
     private ArrayList<Date> days;
     private int currentMonth;
+    private WeakReference<Context> context;
 
-    public CalendarAdapter(Context context, ArrayList<Date> days)
-    {
+    public CalendarAdapter(Context context, ArrayList<Date> days) {
         super(context, R.layout.schedule_calendar_day, days);
         this.days = days;
+        this.context = new WeakReference<>(context);
         inflater = LayoutInflater.from(context);
     }
 
@@ -35,12 +39,13 @@ public class CalendarAdapter extends ArrayAdapter<Date> {
     }
 
     @Override
-    public View getView(int position, View view, ViewGroup parent)
-    {
+    public View getView(int position, View view, ViewGroup parent) {
         // day in question
         Date date = days.get(position);
         Calendar c = Calendar.getInstance();
         c.setTime(date);
+
+        Date today = new Date();
 
 
         // inflate item if it does not exist yet
@@ -51,8 +56,14 @@ public class CalendarAdapter extends ArrayAdapter<Date> {
         TextView text = (TextView) view;
         text.setText(String.valueOf(c.get(Calendar.DAY_OF_MONTH)));
 
+        if (DateTimeComparator.getDateOnlyInstance().compare(c.getTime(), today) == 0) {
+            text.setBackground(context.get().getDrawable(R.drawable.current_date_selector));
+        } else {
+            text.setBackgroundResource(0);
+        }
+
         //if month equals to current month then set text color to black
-        if(currentMonth == c.get(Calendar.MONTH)){
+        if (currentMonth == c.get(Calendar.MONTH)) {
             text.setTextColor(Color.BLACK);
         } else { //else grey
             text.setTextColor(Color.GRAY);
--- app/src/main/java/com/dorm/muro/dormitory/presentation/schedule/ScheduleFragment.java
+++ app/src/main/java/com/dorm/muro/dormitory/presentation/schedule/ScheduleFragment.java
@@ -1,20 +1,24 @@
 package com.dorm.muro.dormitory.presentation.schedule;
 
-
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.ShapeDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.dorm.muro.dormitory.R;
-import com.savvi.rangedatepicker.CalendarPickerView;
+import com.dorm.muro.dormitory.ScheduleCalendar.ScheduleCalendarView;
 
-import java.util.Calendar;
-import java.util.Date;
+import java.util.List;
 
 import butterknife.BindView;
+import butterknife.BindViews;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 /**
@@ -22,6 +26,14 @@ import butterknife.ButterKnife;
  */
 public class ScheduleFragment extends Fragment {
 
+    @BindView(R.id.schedule_calendar)
+    ScheduleCalendarView scheduleCalendarView;
+    @BindViews({R.id.tv_schedule_room_1, R.id.tv_schedule_room_2, R.id.tv_schedule_room_3, R.id.tv_schedule_room_4})
+    List<TextView> mRooms;
+    @BindView(R.id.tv_schedule_commentary)
+    TextView mRoomCommentary;
+
+    private int currentSelectedRoom;
 
     public ScheduleFragment() {
         // Required empty public constructor
@@ -33,8 +45,19 @@ public class ScheduleFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_schedule, container, false);
+        ButterKnife.bind(this, view);
 
+        currentSelectedRoom = 0;
         return view;
     }
 
+    @OnClick({R.id.tv_schedule_room_1, R.id.tv_schedule_room_2, R.id.tv_schedule_room_3, R.id.tv_schedule_room_4})
+    public void selectRoom(TextView mRoom) {
+        mRooms.get(currentSelectedRoom).setBackgroundResource(0);
+        mRooms.get(currentSelectedRoom).setTextColor(Color.BLACK);
+        mRoom.setBackgroundResource(R.drawable.selected_room);
+        mRoom.setTextColor(getResources().getColor(R.color.room_text));
+        currentSelectedRoom = Integer.parseInt(mRoom.getText().toString()) - 1;
+        mRoomCommentary.setText("Set commentary for room" + mRoom.getText().toString());
+    }
 }
