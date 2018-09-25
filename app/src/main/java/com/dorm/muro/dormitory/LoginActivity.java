26d4a91348dccad0f1f7e416485aae5a09ad0467
Mitch <ulamog345@gmail.com>
2018-09-25 20:41:34 +0300

    Base login layout + change animations

--- app/src/main/java/com/dorm/muro/dormitory/LoginActivity.java
+++ app/src/main/java/com/dorm/muro/dormitory/LoginActivity.java
@@ -5,8 +5,12 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ViewFlipper;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -24,6 +28,18 @@ public class LoginActivity extends AppCompatActivity {
     @BindView(R.id.et_login_password)
     public EditText mPasswordEditText;
 
+    @BindView(R.id.vf_login_flipper)
+    ViewFlipper mViewFlipper;
+
+    @BindView(R.id.ll_login_header)
+    LinearLayout mLoginHeader;
+
+    @BindView(R.id.tv_login_register_stage1)
+    TextView mRegisterStage1;
+
+    @BindView(R.id.tv_login_register_stage2)
+    TextView mRegisterStage2;
+
     private SharedPreferences preferences;
     private Intent mainActivityIntent;
 
@@ -35,22 +51,17 @@ public class LoginActivity extends AppCompatActivity {
 
         preferences = getSharedPreferences(SHARED_PREFERENCES, MODE_PRIVATE);
         mainActivityIntent = new Intent(getApplicationContext(), MainActivity.class);
+        mViewFlipper.setInAnimation(this, android.R.anim.slide_in_left);
+        mViewFlipper.setOutAnimation(this, android.R.anim.slide_out_right);
 
-        if(preferences.getBoolean(IS_LOGGED, false)){
+        if (preferences.getBoolean(IS_LOGGED, false)) {
             startActivity(mainActivityIntent);
         }
     }
 
     @OnClick(R.id.btn_login_login)
-    public void signUp() {
-        //TODO: create sign up method
-        Toast.makeText(getApplicationContext(), "Create sign up method", Toast.LENGTH_SHORT).show();
-
-    }
-
-    @OnClick(R.id.tv_login_create_account)
-    public void signIn(){
-        if(checkUser()){
+    public void signIn() {
+        if (checkUser()) {
             preferences.edit()
                     .putBoolean(IS_LOGGED, true)
                     .apply();
@@ -58,7 +69,26 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-    private boolean checkUser(){
+    @OnClick(R.id.tv_login_create_account)
+    public void proceedToRegisterFirstPage() {
+        mViewFlipper.showNext();
+        mLoginHeader.setVisibility(View.VISIBLE);
+        mRegisterStage1.setBackgroundColor(getResources().getColor(R.color.register_stage));
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mViewFlipper.getDisplayedChild() == 0) {
+            finish();
+        } else {
+            mViewFlipper.showPrevious();
+            if (mViewFlipper.getDisplayedChild() == 0) {
+                mLoginHeader.setVisibility(View.INVISIBLE);
+            }
+        }
+    }
+
+    private boolean checkUser() {
         //TODO: create check user method
         Toast.makeText(getApplicationContext(), "create check user method", Toast.LENGTH_SHORT).show();
         return mLoginEditText.getText().toString().equals("login") && mPasswordEditText.getText().toString().equals("pass");
